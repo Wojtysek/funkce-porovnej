9b39566f61d64073bacf73fa7850bee39be5e82f
Wojtysek <vojtahr1@post.cz>
2022-05-16 14:11:21 +0200

    funkcni

--- funkce porovnej/funkce porovnej.cpp
+++ funkce porovnej/funkce porovnej.cpp
@@ -1,22 +1,29 @@
 ﻿#include <stdio.h>
 
 int porovnej(char* x, char* y) {
-	unsigned long long delsi = strlen(x);
-	unsigned long long kratsi = strlen(y);
+	unsigned long long delsi = delka(x);
+	unsigned long long kratsi = delka(y);
 	//v promene delsi -> delka delsiho retezce
 	if (kratsi > delsi) delsi = kratsi;
 
 	for (unsigned long long i = 0; i <= delsi; i++) {
-
+		if (*(x + i) != *(y + i)) return 1; //nejsou-li shodne -> koncime
 	}
-
+	//jsou shodne
 	return 0;
 }
+
+int delka(char* x) {
+	unsigned long long i = 0;
+	while (*(x + i) != '\0') i++;
+	return i;
+}
+
 
 int main(void) {
 
 	char jedna[] = "zdarec";
-	char dva[] = "nazdar";
+	char dva[] = "zdarec";
 	if (porovnej(jedna, dva) == 0) {
 		printf("retezce jsou stejne\n");
 	}
