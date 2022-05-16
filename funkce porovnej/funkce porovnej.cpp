cae82801e16c6266c08bf9e5f4cd75cea73d90af
Wojtysek <vojtahr1@post.cz>
2022-05-16 13:52:59 +0200

    Přidejte soubory projektu.

---
+++ funkce porovnej/funkce porovnej.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int porovnej(char* x, char* y) {
+	unsigned long long delsi = strlen(x);
+	unsigned long long kratsi = strlen(y);
+	//v promene delsi -> delka delsiho retezce
+	if (kratsi > delsi) delsi = kratsi;
+
+	for (unsigned long long i = 0; i <= delsi; i++) {
+
+	}
+
+	return 0;
+}
+
+int main(void) {
+
+	char jedna[] = "zdarec";
+	char dva[] = "nazdar";
+	if (porovnej(jedna, dva) == 0) {
+		printf("retezce jsou stejne\n");
+	}
+	else {
+		printf("retezce nejsou stejne\n");
+	}
+}
